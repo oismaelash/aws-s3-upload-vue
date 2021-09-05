6e1d6ef932a8bb06a3f93c5f4afd01754562e636
ismaelash <contato@ismaelnascimento.com>
2021-09-05 18:40:19 -0300

    initial commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,55 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <input type="file" @change="onChangeFile" />
+    <br />
+    <br />
+    <button @click="handleSendFile">send file</button>
   </div>
 </template>
 
 <script>
+import AWSS3UploadAshClient from "aws-s3-upload-ash";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  name: "HelloWorld",
+  data: function () {
+    return {
+      fileSelected: null,
+      config: {
+        bucketName: "aws-s3-upload-ash",
+        dirName:
+          "media" /* optional - when use: e.g BUCKET_ROOT/dirName/fileName.extesion */,
+        region: "us-east-1",
+        accessKeyId: process.env.VUE_APP_AWS_ACCESS_KEY,
+        secretAccessKey: process.env.VUE_APP_AWS_SECRET_KEY,
+        s3Url: "https://aws-s3-upload-ash.s3.amazonaws.com/",
+      },
+    };
+  },
+  methods: {
+    onChangeFile: function (event) {
+      console.log("fileSelected", event.target.files[0]);
+      this.fileSelected = event.target.files[0];
+    },
+    handleSendFile: function () {
+      console.log(
+        "process.env.VUE_APP_AWS_ACCESS_KEY",
+        process.env.VUE_APP_AWS_ACCESS_KEY
+      );
+      console.log("send file", this.fileSelected);
+      let S3CustomClient = new AWSS3UploadAshClient(this.config);
+      S3CustomClient.uploadFile(
+        this.fileSelected,
+        this.fileSelected.type,
+        undefined,
+        this.fileSelected.name,
+        undefined
+      )
+        .then((data) => console.log(data))
+        .catch((err) => console.error(err));
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
